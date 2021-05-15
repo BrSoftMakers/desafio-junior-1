c8c9c3bef1b221302978faeaecab79bc444bcab5
Luciene <lucienebsousa@gmail.com>
2021-05-15 16:24:05 -0300

    start Project

---
+++ frontendReact/petshop-softmakers/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div>Petshop SoftMakers em breve...</div>;
+}
+
+export default App;
---
+++ frontendReact/petshop-softmakers/src/Routes/Routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+export default function Router() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
