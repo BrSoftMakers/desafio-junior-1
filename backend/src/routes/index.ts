5ae931a3c1c77098225d72f141589d4d6c4b37ba
Julio Lemos <juliolemos_@outlook.com>
2021-09-27 21:39:29 -0300

    feat: create route system

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { Request, Response } from 'express';
+
+export const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.send("Typescript + Express")
+});
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { Request, Response } from 'express';
+
+import { router } from './routes';
 
 const app = express();
 const PORT = 3333 || process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => res.send('Express + TypeScript Server'));
+app.use('/', router)
 
 app.listen(PORT, () => {
   console.log(`🔥[server]: Server is burning at port ${PORT}`);
