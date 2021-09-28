e63d603fbf3e2d378aa48746ac78bd83907db86d
Julio Lemos <juliolemos_@outlook.com>
2021-09-27 22:44:34 -0300

    feat: apply methods HTTP to routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,42 @@
-import 'reflect-metadata';
-
-import { Router } from 'express';
-import { Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
+import { createConnection } from 'typeorm';
+import { Pets } from "./../database/entity/pets.entity"
 
 export const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send("Typescript + Express")
-});
+createConnection().then(connection => {
+  const petsRepository = connection.getRepository(Pets);
+
+  router.get("/pets", async (req: Request, res: Response) => {
+    const pets = await petsRepository.find()
+    res.json(pets)
+  });
+
+  router.get("/pets/:id", async (req: Request, res: Response) => {
+    const results = await petsRepository.findOne(req.params.id);
+    return res.send(results)
+  })
+
+  router.post("/pets", async (req: Request, res: Response) => {
+    const pet = await petsRepository.create(req.body)
+    const results = await petsRepository.save(pet)
+    return res.send(results)
+  })
+
+  router.put("/pets/:id", async (req: Request, res: Response) => {
+    const pet = await petsRepository.findOne(req.params.id);
+    petsRepository.merge(pet as Pets, req.body);
+    const results = await petsRepository.save(pet as Pets)
+    return res.send(results)
+  })
+
+  router.delete("/pets/:id", async (req: Request, res: Response) => {
+    const results = await petsRepository.delete(req.params.id);
+    return res.send(results)
+  })
+
+})
+
+
+
+
