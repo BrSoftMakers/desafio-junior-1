17cf28c0b75c4f34db91948aea15fa56e1a27026
Julio Lemos <juliolemos_@outlook.com>
2021-09-27 22:39:11 -0300

    chore: add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,15 @@
+import 'reflect-metadata';
+
 import express from 'express';
+import cors from "cors"
 
 import { router } from './routes';
 
 const app = express();
+
+app.use(express.json())
+app.use(cors())
+
 const PORT = 3333 || process.env.PORT;
 
 app.use('/', router)
