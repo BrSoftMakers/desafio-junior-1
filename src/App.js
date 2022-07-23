f837c3504a7f78569443ae2c82f5e69365856409
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-22 22:12:18 -0300

    created themeProvider | globalStyles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+// CSS
+import { GlobalStyles } from "./styles/global-styles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+
 function App() {
-  return <>Olá mundo</>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <h1>oi</h1>
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    bgColor: "#f6fcff",
+    bgColorDarker: "edf3f6",
+  },
+  fonts: {
+    family: {
+      default: "'Roboto', sans-serif",
+      fontSecundary: "'Poppins', sans-serif",
+      fontOptional: "'Teko', sans-serif",
+    },
+  },
+};
