058a19c9a443494b020caa7e489fa67a6a90235c
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-07-23 11:59:58 -0300

    updated navbar style

--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -25,6 +25,7 @@ export const LeftContainer = styled.div`
 export const RightContainer = styled.div`
   flex: 70%;
   display: flex;
+  align-items: center;
   justify-content: flex-end;
   padding-right: 5%;
 `;
@@ -37,10 +38,27 @@ export const NavbarLink = styled(Link)`
   font-size: 2rem;
   padding: 10px;
   text-decoration: none;
+  display: flex;
+
+  @media (max-width: 700px) {
+    font-size: 1.5rem;
+  }
+
+  @media (max-width: 450px) {
+    font-size: 1.2rem;
+  }
 `;
 
 export const NavbarLinkRight = styled(NavbarLink)`
-  font-size: 1.5rem;
+  font-size: 1rem;
+
+  @media (max-width: 700px) {
+    font-size: 0.9rem;
+  }
+
+  @media (max-width: 450px) {
+    font-size: 0.8rem;
+  }
 `;
 
 export const Span = styled.span`
@@ -49,7 +67,8 @@ export const Span = styled.span`
       color: ${theme.colors.defaultGreen};
       font-weight: bolder;
       text-transform: uppercase;
-      margin-left: 0.3rem;
+      margin-left: 0.2rem;
+      display: inline-block;
     }
   `}
 `;
