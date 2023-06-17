0727a101830a62f006a42a35feadf88a47023ad2
Luis Eduardo <luiseduess@gmail.com>
2023-06-17 17:19:13 -0300

    feat: create migration for table animals

---
+++ backend/src/database/migrations/20230617200511-create-animals-table.ts
@@ -0,0 +1,46 @@
+import { DataTypes, QueryInterface, fn } from 'sequelize'
+
+module.exports = {
+  up: async (queryInterface: QueryInterface, dataTypes: typeof DataTypes) => {
+    queryInterface.createTable('animals', {
+      id: {
+        type: dataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+      },
+      name: {
+        type: dataTypes.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: dataTypes.INTEGER,
+        allowNull: false,
+      },
+      type: {
+        type: dataTypes.ENUM,
+        values: ['dog', 'cat'],
+        allowNull: true,
+      },
+      race: {
+        type: dataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: dataTypes.DATE,
+        allowNull: false,
+        defaultValue: fn('NOW'),
+      },
+      updatedAt: {
+        type: dataTypes.DATE,
+        allowNull: false,
+        defaultValue: fn('NOW'),
+      },
+    })
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.dropTable('animals')
+  },
+}
