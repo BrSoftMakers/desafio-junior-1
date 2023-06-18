c28f7345126675a61801147773f1a41cc6bf4e53
Luis Eduardo <luiseduess@gmail.com>
2023-06-18 02:24:28 -0300

    initializing frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <h1>START PROJECT</h1>
+  </React.StrictMode>
+)
