011149c578bcf444b2f7d637fddf93d475e8f7c3
Luis Eduardo <luiseduess@gmail.com>
2023-06-19 11:19:22 -0300

    feat: create pagination to CustomersListing page

--- frontend/src/services/apiPetsAndCustomers/CustomerServices/CustomerServices.ts
+++ frontend/src/services/apiPetsAndCustomers/CustomerServices/CustomerServices.ts
@@ -2,11 +2,7 @@ import { api } from '../config'
 import { ICustomer } from '../types'
 
 export class CustomerServices {
-  static async getAllCustomers(
-    page: number,
-    limit: number,
-    searchFilter: string
-  ) {
+  static async getAllCustomers(page: number, limit: number, searchFilter = '') {
     const url = `/customers?page=${page}&limit=${limit}&searchFilter=${searchFilter}`
     const customers = await api.get<Omit<ICustomer, 'customerAddress'>[]>(url)
     return customers.data
