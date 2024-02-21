381e12056983ad588f801081d733a20a3aa13cf1
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-02-21 08:13:49 -0300

    sestrutura basica criada / basic structure created

---
+++ softpet-project/src/app/app.js
@@ -0,0 +1,10 @@
+import styles from "../app.module.css";
+import Header from '../components/header'
+
+export default function App() {
+  return (
+    <div className={styles.main}>
+     <Header/>
+    </div>
+  );
+}
