d7b0d41b1b1b983145c40294a21d48afb80b2539
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-02-23 02:08:42 -0300

    values dos input feito e funcionando / values forms done

--- softpet-project/api.ts
+++ softpet-project/api.ts
@@ -3,11 +3,11 @@ import { IPets } from './types/pets'
 const BaseURL = "http://localhost:5000";
 
 export const getAllPets = async (): Promise<IPets[]> => {
-    const res = await fetch(`${BaseURL}/pets`);
+    const res = await fetch(`${BaseURL}/pets`,{cache:'no-store'});
     const pets = await res.json();
     return pets
 }
-export const addPets = async(pet):Promise<IPets> =>{
+export const addPets = async(pet:IPets):Promise<IPets> =>{
     const res = await fetch(`${BaseURL}/pets`,{
         method:'POST',
         headers:{
--- softpet-project/src/app/components/cadastrarPet.tsx
+++ softpet-project/src/app/components/cadastrarPet.tsx
@@ -9,16 +9,43 @@ import VoltarCadastroIcon from './icons/VoltarCadastroIcon.svg'
 import SelectAnimalIcon from './icons/SelectAnimalIcon.svg';
 import TelefoneIcon from './icons/TelefoneIcon.svg'
 import DataIcon from './icons/DataIcon.svg';
+import { IPets } from '../../../types/pets';
+import { addPets } from "../../../api";
+import { useRouter } from "next/navigation";
 
 export default function CadastrarPet({ }) {
-
+    const router = useRouter()
     const [abrirCadastro, setAbrirCadastro] = useState<boolean>(false);
-    const [newPetValue, setNewPetValue] = useState<string>('')
+    const [newPetValue, setNewPetValue] = useState<IPets>({
+        id: "",
+        nome: "",
+        animal: "",
+        dono: "",
+        raca: "",
+        telefone: "",
+        nascimento: ""
+    });
 
-    const handleSubmitPets: FormEventHandler<HTMLFormElement> = (e) => {
+    const handleSubmitPets: FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
-        console.log("nome: ",newPetValue)
+        await addPets(newPetValue);
+        setNewPetValue({
+            id: "",
+            nome: "",
+            animal: "",
+            dono: "",
+            raca: "",
+            telefone: "",
+            nascimento: ""
+        });
+        setAbrirCadastro(false) 
+        console.log(newPetValue)
+        router.refresh()
+
     }
+    const handleAnimalChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewPetValue({ ...newPetValue, animal: e.target.value });
+    };
     return (
         <div className={styles.cadastrarPetForm}>
 
@@ -34,8 +61,8 @@ export default function CadastrarPet({ }) {
 
                         <span> <PetNameIcon /><label>Nome</label></span>
                         <input
-                            value={newPetValue}
-                            onChange={e => setNewPetValue(e.target.value)}
+                            value={newPetValue.nome}
+                            onChange={e => setNewPetValue({ ...newPetValue, nome: e.target.value })}
                             placeholder="Nome Sobrenome" type="text" />
 
                     </div>
@@ -44,37 +71,59 @@ export default function CadastrarPet({ }) {
                         <span><SelectAnimalIcon /><label>Animal</label></span>
                         <div className={styles.ContainerSelectAnimal}>
                             <span className={styles.selectAnimal}>
-                                <input type="radio" name="grupo1" id="opcao1" />
+                                <input
+                                    onChange={handleAnimalChange}
+                                    type="radio" id="opcao1"
+                                    name="animal"
+                                    value="cachorro"
+                                    checked={newPetValue.animal === "cachorro"} />
                                 <label htmlFor="opcao1">Cachorro</label>
                             </span>
                             <span className={styles.selectAnimal}>
-                                <input type="radio" name="grupo1" id="opcao2" />
+                                <input
+                                    onChange={handleAnimalChange} 
+                                    type="radio" id="opcao2"
+                                    name="animal"
+                                    value="gato"
+                                    checked={newPetValue.animal === "gato"} />
                                 <label htmlFor="opcao2">Gato</label>
                             </span>
                         </div>
                     </div>
                     <div className={styles.infoCadastro_Dono}>
                         <span> <PetDonoIcon /><label>Dono</label></span>
-                        <input placeholder="Nome Sobrenome" type="text" />
+                        <input
+                            value={newPetValue.dono}
+                            onChange={e => setNewPetValue({ ...newPetValue, dono: e.target.value })}
+                            placeholder="Nome Sobrenome" type="text" />
                     </div>
-                    <div className={styles.infoCadastro_Raça}>
+                    <div className={styles.infoCadastro_raca}>
                         <span><SelectAnimalIcon /><label>Raça</label></span>
-                        <input placeholder="Raça" type="text" />
+                        <input
+                            value={newPetValue.raca}
+                            onChange={e => setNewPetValue({ ...newPetValue, raca: e.target.value })}
+                            placeholder="Raça" type="text" />
                     </div>
                     <div className={styles.infoCadastro_Telefone}>
                         <span><TelefoneIcon /><label>Telefone</label></span>
-                        <input placeholder="(00) 0 0000-0000" type="tel" name="" id="" />
+                        <input
+                            value={newPetValue.telefone}
+                            onChange={e => setNewPetValue({ ...newPetValue, telefone: e.target.value })}
+                            placeholder="(00) 0 0000-0000" type="tel" name="" id="" />
                     </div>
                     <div className={styles.infoCadastro_Nascimento}>
                         <span><DataIcon /><label>Nascimento</label></span>
-                        <input placeholder="22/08/2020" type="datetime" name="" id="" />
+                        <input
+                            value={newPetValue.nascimento}
+                            onChange={e => setNewPetValue({ ...newPetValue, nascimento: e.target.value })}
+                            placeholder="22/08/2020" type="datetime" name="" id="" />
                     </div>
                     <footer className={styles.footerCadastroPet}>
-                    <button onClick={() => setAbrirCadastro(false)} className={styles.buttonVoltarCadastroPet}>
+                        <button onClick={() => setAbrirCadastro(false)} className={styles.buttonVoltarCadastroPet}>
                             <span><VoltarCadastroIcon /><span style={{ paddingLeft: 5 }}>Voltar</span></span>
-                        </button> 
+                        </button>
 
-                    <button type="submit" className={styles.buttonCadastrarPet}>
+                        <button type="submit" className={styles.buttonCadastrarPet}>
                             <span><CadastroIcon />
                                 <span style={{ paddingLeft: 5 }}>Cadastrar</span>
                             </span>
