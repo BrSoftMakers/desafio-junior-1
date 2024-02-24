ffcfa2d6742789bf22f8f2866590db3419592c87
Erikles Geyzon <geyzon100@gmail.com>
2024-02-24 11:18:55 -0300

    feat: creating alias for svg files and components files

--- soft-pet-frontend/src/app/modules/pets/pages/index.tsx
+++ soft-pet-frontend/src/app/modules/pets/pages/index.tsx
@@ -1,15 +1,16 @@
-import Button from "../../chore/components/button"
-import SearchBar from "../../chore/components/searchBar"
+import { AddIcon, Logo, TrashIcon } from "@icons/index"
+import { Button, SearchBar } from "@components/index"
 import Main from "../styles/pet-page"
 
 export default function PetPage() {
     return(
         <Main>
+            <Logo />
             <SearchBar mr="22px"/>
-            <Button variant="PRIMARY"/>
+            <Button variant="PRIMARY" />
             <Button variant="SECONDARY"/>
             <Button />
-            <Button variant="DANGER"/>
+            <Button variant="DANGER" icon={<TrashIcon />}/>
         </Main>
     )
 }
